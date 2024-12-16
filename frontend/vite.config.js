6ca3f4731c60502b287ce5c121d22c16c47c2e33
sahill-07 <sahilbangar72002@gmail.com>
2024-12-16 22:40:47 +0530

    Updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
