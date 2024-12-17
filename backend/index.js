6c7b97d6d591b694bcb67bb2ee0283f6fadad2e7
sahill-07 <sahilbangar72002@gmail.com>
2024-12-17 12:36:15 +0530

    Updates

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
+const _dirname = path.resolve()
 
 // middleware
 app.use(express.json());
@@ -20,6 +22,11 @@ const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
 }
+
+app.use(express.static(path.join(_dirname , "/frontend/dist")));
+app.get('*' ,  (_ , res) => {
+    res.sendFile(path.resolve(_dirname , "frontend" , "dist" , "index.html"))
+});
 
 app.use(cors(corsOptions));
 
